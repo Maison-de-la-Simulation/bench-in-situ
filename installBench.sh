b4aa4e4616e0a8b5b398a39a428035722cc85302
P-A Raclius <linux@LAPTOP-LL714JBN>
2024-01-31 16:19:59 +0100

    Install script

---
+++ installBench.sh
@@ -0,0 +1,13 @@
+mkdir benchInSitu
+~/miniconda3/bin/conda create python=3.7 --prefix ./condaEnv
+conda activate ./
+
+git clone --recurse-submodules https://github.com/Maison-de-la-Simulation/bench-in-situ.git
+cd bench-in-situ/lib/pdi
+
+git checkout deisa_plugin
+sed -i 's/project(pdi_deisa_plugin LANGUAGES CXX)/project(pdi_deisa_plugin LANGUAGES C CXX)/' plugins/deisa/CMakeLists.txt
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/../../install_pdi -DUSE_HDF5=EMBEDDED -DBUILD_HDF5_PARALLEL=ON  -DUSE_yaml=EMBEDDED -DUSE_paraconf=EMBEDDED -DBUILD_SHARED_LIBS=ON -DBUILD_FORTRAN=OFF -DBUILD_BENCHMARKING=OFF -DBUILD_SET_VALUE_PLUGIN=OFF -DBUILD_TESTING=OFF -DBUILD_DECL_NETCDF_PLUGIN=OFF -DBUILD_USER_CODE_PLUGIN=ON -DBUILD_DEISA_PLUGIN=ON -DBUILD_PYTHON=ON ..
+
