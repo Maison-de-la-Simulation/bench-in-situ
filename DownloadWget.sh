06d819aefca30d64fe56c2ac5a7f5bf518b60ea7
P-A Raclius <linux@LAPTOP-LL714JBN>
2024-02-01 15:05:16 +0100

    Deleted old scripts

--- DownloadWget.sh
+++
@@ -1,9 +0,0 @@
-cd lib
-wget https://github.com/kokkos/kokkos/archive/refs/tags/4.2.00.tar.gz
-gunzip 4.2.00.tar.gz
-tar -xf 4.2.00.tar
-
-wget https://github.com/benhoyt/inih/archive/refs/tags/r58.tar.gz
-gunzip r58.tar.gz
-tar -xf r58.tar
-
