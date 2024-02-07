a6f130d884932f33f61642d908af67e8cd46d4bb
P-A Raclius <linux@LAPTOP-LL714JBN>
2024-02-07 11:23:09 +0100

    offline scripts

---
+++ offline_env_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source env.sh
+print_env
+
+mkdir -p ${WORKING_DIR}
+cd ${WORKING_DIR}
+
+# setup python environment
+python -m venv ${PYTHON_ENV}
+
+# activate python environment
+source ${PYTHON_ENV}/bin/activate
+
+pip install deps/*.whl
+pip install deps/*.zip
+
+# install Deisa
+pip install --no-index --no-build-isolation --no-deps ../lib/deisa
+
+# TODO: offline setup
+
+pip download ../lib/deisa/ -d deps/
+pip download wheel versioneer -d deps/
+
+
+cd --
