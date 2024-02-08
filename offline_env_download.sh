e80a0de5a1ee793bce2a78444815e0ad889a1a58
Benoit Martin <benoit.martin2@cea.fr>
2024-02-08 11:17:27 +0100

    add TODO comments for future work

--- offline_env_download.sh
+++ offline_env_download.sh
@@ -6,6 +6,16 @@ print_env
 mkdir -p ${WORKING_DIR}
 cd ${WORKING_DIR}
 
+# TODO: create conda python 3.9
+# 1) create conda env with python 3.9: conda create -y --name py39 python=3.9.10
+# 2) activate environment
+# 3) pip download
+# 4) tar gz -> deisa_deps_py39.tar.gz
+# 5) cleanup: deactivate conda env, rm conda env, downloaded files
+
+# TODO: create conda python 3.10
+# TODO: create conda python 3.11
+
 # setup python environment
 python -m venv ${PYTHON_ENV}
 
@@ -23,3 +33,4 @@ pip download wheel versioneer -d deps/
 deactivate
 rm -r ${PYTHON_ENV}
 cd --
+
