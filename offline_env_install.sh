d9e7291941d41dd5ddacbfd5698bea8513748dea
P-A Raclius <linux@LAPTOP-LL714JBN>
2024-02-09 10:15:58 +0100

    Install changes

--- offline_env_install.sh
+++ offline_env_install.sh
@@ -17,4 +17,13 @@ pip install deps/*.whl
 # install Deisa
 pip install --no-index --no-build-isolation --no-deps ../lib/deisa
 
+echo "TESTING DEPENDENCIES"
+echo "Deisa :"
+python -m import deisa
+echo "Numpy :"
+python -m import numpy
+echo "Dask :"
+python -m import Dask
+echo "Distributed :"
+python -m import distributed
 cd --
