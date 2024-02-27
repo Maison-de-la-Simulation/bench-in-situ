89674f0025b8f1d418aec28e34a1a6e721af723f
Benoit Martin <benoit.martin2@cea.fr>
2024-02-27 15:39:32 +0100

    add working config for ruche

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -8,4 +8,9 @@ source env.sh
 ./build_pdi.sh
 ./build_simulation.sh
 
+# copy files to working directory
+cd ..
+cp deisa_deps_py* ${WORKING_DIR}
+cp -r in-situ ${WORKING_DIR}
+#cp io.yml io_chkpt.yml io_deisa.yml ${WORKING_DIR}
 
--- scripts/launch_simulation.sh
+++ scripts/launch_simulation.sh
@@ -3,22 +3,25 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd) && cd ${SCRIPT_DIR}
 source env.sh
 print_env
-source lib/install_pdi/share/pdi/env.sh
+source ${WORKING_DIR}/${PYTHON_ENV}/bin/activate
+source ${PDI_INSTALL_DIR}/share/pdi/env.sh
 
 
 export PYTHONPATH=${WORKING_DIR}/${PYTHON_ENV}/lib/python3.11/site-packages
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/.local/lib/
+#export LD_LIBRARY_PATH=${PDI_INSTALL_DIR}/lib:${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+#export LD_PRELOAD=${PDI_INSTALL_DIR}/lib64/libyaml.so # note: only for mac
 
 echo "PYTHONPATH=${PYTHONPATH}"
 echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+#echo "LD_PRELOAD=${LD_PRELOAD}"
 
-cp io_chkpt.yml ${BUILD_DIR}
-cp setup.ini ${BUILD_DIR}
+cp ../io_chkpt.yml ../io_deisa.yml ${BUILD_DIR}
+cp ../setup.ini ${BUILD_DIR}
+
 cd ${BUILD_DIR}
 
-export LD_PRELOAD=${WORKING_DIR}/install_pdi/lib64/libyaml.so
-
-./main ./setup.ini ./io_chkpt.yml
+./main setup.ini io_deisa.yml
 
 #pdirun mpirun -np 1 ${BUILD_DIR}/main ../setup.ini ../io_deisa.yml
 cd --
