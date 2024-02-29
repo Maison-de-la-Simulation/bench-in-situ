18125b58b0cc637729492aba6b9650d8a782bc9f
yushan.wang <yushan.wang@cea.fr>
2024-02-29 14:17:13 +0100

    change to GPU targeted build

--- scripts/build_simulation.sh
+++ scripts/build_simulation.sh
@@ -9,7 +9,17 @@ cd ${WORKING_DIR}
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-cmake -DCMAKE_BUILD_TYPE=Release -DSESSION=MPI_SESSION -DKokkos_ENABLE_OPENMP=ON -DEuler_ENABLE_PDI=ON ../..
+#cmake -DCMAKE_BUILD_TYPE=Release -DSESSION=MPI_SESSION -DKokkos_ENABLE_OPENMP=ON -DEuler_ENABLE_PDI=ON ../..
+
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DEuler_ENABLE_PDI=ON \
+    -DKokkos_ENABLE_SERIAL=ON \
+    -DKokkos_ENABLE_CUDA=ON \
+    -DKokkos_ARCH_AMPERE80=ON \
+    -DSESSION=MPI_SESSION \
+    ../..
 
 make -j8
 
