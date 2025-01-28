ad13cca6de26546b7c19a3065d676f20c3312b8f
Benoit Martin <bmartin@cea.fr>
2025-01-28 16:49:53 +0100

    WIP: include new deisa

--- scripts/offline_python_env_download.sh
+++ scripts/offline_python_env_download.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd) && cd
 source env.sh
 print_env
 
-py_versions=( "3.9" "3.10" "3.11" "3.12" "3.13" )
+# Old Deisa
+#py_versions=( "3.9" "3.10" "3.11" "3.12" "3.13" )
+py_versions=( "3.10" )
 for py_version in "${py_versions[@]}"; do
     echo "Working on python version: ${py_version}"
     docker run -it --rm -w /root/workingdir -v ${SCRIPT_DIR}/..:/root/workingdir python:${py_version} bash -c "pip install --upgrade pip && \
@@ -13,6 +15,18 @@ for py_version in "${py_versions[@]}"; do
     cd ${PYTHON_WORKING_DIR} && pip wheel ${PYTHON_WORKING_DIR}/distributed-2021.11.2+1398.g9b7ce185.zip && cd - && \
     rm ${PYTHON_WORKING_DIR}/distributed-2021.11.2+1398.g9b7ce185.zip && \
     tar -cvzf output/deisa_deps_py${py_version//.}.tar.gz -C ${PYTHON_WORKING_DIR} . && \
+    rm -rf ${PYTHON_WORKING_DIR}"
+done
+
+# New Deisa
+#py_versions=( "3.9" "3.10" "3.11" "3.12" "3.13" )
+py_versions=( "3.10" )
+for py_version in "${py_versions[@]}"; do
+    echo "Working on python version: ${py_version}"
+    docker run -it --rm -w /root/workingdir -v ${SCRIPT_DIR}/..:/root/workingdir python:${py_version} bash -c "pip install --upgrade pip && \
+    pip download lib/new_deisa -d ${PYTHON_WORKING_DIR} && \
+    pip download wheel versioneer zarr cytools h5py bokeh -d ${PYTHON_WORKING_DIR} && \
+    tar -cvzf output/new_deisa_deps_py${py_version//.}.tar.gz -C ${PYTHON_WORKING_DIR} . && \
     rm -rf ${PYTHON_WORKING_DIR}"
 done
 
--- scripts/offline_python_env_setup.sh
+++ scripts/offline_python_env_setup.sh
@@ -6,6 +6,11 @@ print_env
 
 cd ${WORKING_DIR}
 
+
+echo "#######################"
+echo "# Old Deisa"
+echo "#######################"
+
 # setup python environment
 python3 -m venv ${PYTHON_ENV}
 
@@ -13,23 +18,63 @@ python3 -m venv ${PYTHON_ENV}
 source ${PYTHON_ENV}/bin/activate
 pip install --upgrade pip
 
-
-tar xvf deisa_deps_py${PYTHON_VERSION}.tar.gz
-pip install *.whl
+# untar and install dependencies
+mkdir tmp_untar
+tar xvf deisa_deps_py${PYTHON_VERSION}.tar.gz -C tmp_untar
+pip install tmp_untar/*.whl
+rm -rf tmp_untar
 
 # install Deisa
 pip install --no-index --no-build-isolation --no-deps ../lib/deisa
 
 echo "TESTING DEPENDENCIES"
 echo "Deisa.."
-python -c "import deisa"
+python3 -c "import deisa"
 echo "HDF5.."
-python -c "import h5py"
+python3 -c "import h5py"
 echo "Numpy.."
-python -c "import numpy"
+python3 -c "import numpy"
 echo "Dask.."
-python -c "import dask"
+python3 -c "import dask"
 echo "Distributed.."
-python -c "import distributed"
+python3 -c "import distributed"
+
+deactivate
+
+
+echo "#######################"
+echo "# New Deisa"
+echo "#######################"
+PYTHON_ENV="${PYTHON_ENV}_new"
+
+# setup python environment
+python3 -m venv ${PYTHON_ENV}
+
+# activate python environment
+source ${PYTHON_ENV}/bin/activate
+pip install --upgrade pip
+
+# untar and install dependencies
+mkdir tmp_untar
+tar xvf new_deisa_deps_py${PYTHON_VERSION}.tar.gz -C tmp_untar
+pip install tmp_untar/*.whl
+rm -rf tmp_untar
+
+# install Deisa
+pip install --no-index --no-build-isolation --no-deps ../lib/new_deisa
+
+echo "TESTING DEPENDENCIES"
+echo "Deisa.."
+python3 -c "import deisa"
+echo "HDF5.."
+python3 -c "import h5py"
+echo "Numpy.."
+python3 -c "import numpy"
+echo "Dask.."
+python3 -c "import dask"
+echo "Distributed.."
+python3 -c "import distributed"
+
+deactivate
 
 cd --
