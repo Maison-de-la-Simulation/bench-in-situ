d6444f67ad9ff8fdc48645c2470ea0a642b00639
Benoit Martin <bmartin@cea.fr>
2025-01-29 11:31:06 +0100

    update scripts to select new or old deisa version

--- scripts/launch_insitu.sh
+++ scripts/launch_insitu.sh
@@ -4,15 +4,38 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd) && cd
 source env.sh
 print_env
 
+
+run_dask () {
+  dask scheduler --scheduler-file=$SCHEFILE &
+  sleep 3
+  sync
+  dask worker --local-directory /tmp --scheduler-file=$SCHEFILE &
+}
+
+
+
 cd ${WORKING_DIR}
-source ${PYTHON_ENV}/bin/activate
 
-dask scheduler --scheduler-file=$SCHEFILE &
-sleep 3
-sync
+if [ "$1" = "old" ]; then
+  echo "#####################"
+  echo "# Running OLD Deisa #"
+  echo "#####################"
 
-dask worker --local-directory /tmp --scheduler-file=$SCHEFILE &
+  source ${PYTHON_ENV}/bin/activate
+  run_dask
+  python3 -O ../in-situ/fft.py $SCHEFILE
+elif [ "$1" = "new" ]; then
+  echo "#####################"
+  echo "# Running NEW Deisa #"
+  echo "#####################"
+  
+  source ${PYTHON_ENV}_new/bin/activate
+  run_dask
+  python3 -O ../in-situ/fft_new.py $SCHEFILE
+else
+  echo "Unknown option. accepted values are: old, new"
+  exit 1
+fi
 
-python3 -O ../in-situ/fft.py $SCHEFILE
 
 cd --
--- scripts/launch_simulation.sh
+++ scripts/launch_simulation.sh
@@ -3,14 +3,40 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd) && cd ${SCRIPT_DIR}
 source env.sh
 print_env
-source ${WORKING_DIR}/${PYTHON_ENV}/bin/activate
-source ${PDI_INSTALL_DIR}/share/pdi/env.sh
 
 
-export PYTHONPATH=${WORKING_DIR}/${PYTHON_ENV}/lib/python3.11/site-packages:${PYTHONPATH}
-#export LD_LIBRARY_PATH=${PDI_INSTALL_DIR}/lib:${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
-#export LD_PRELOAD=${PDI_INSTALL_DIR}/lib64/libyaml.so # note: only for mac
+if [ "$1" = "old" ]; then
+  echo "#####################"
+  echo "# Running OLD Deisa #"
+  echo "#####################"
+
+  source ${WORKING_DIR}/${PYTHON_ENV}/bin/activate
+  source ${PDI_INSTALL_DIR}/share/pdi/env.sh
+
+  #export PYTHONPATH=${WORKING_DIR}/${PYTHON_ENV}/lib/python3.11/site-packages:${PYTHONPATH}
+  #export LD_LIBRARY_PATH=${PDI_INSTALL_DIR}/lib:${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+  #export LD_LIBRARY_PATH=${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+  #export LD_PRELOAD=${PDI_INSTALL_DIR}/lib64/libyaml.so # note: only for mac
+elif [ "$1" = "new" ]; then
+  echo "#####################"
+  echo "# Running NEW Deisa #"
+  echo "#####################"
+  
+  source ${WORKING_DIR}/${PYTHON_ENV}_new/bin/activate
+  source ${PDI_INSTALL_DIR}/share/pdi/env.sh
+
+  #export PYTHONPATH=${WORKING_DIR}/${PYTHON_ENV}/lib/python3.11/site-packages:${PYTHONPATH}
+  #export LD_LIBRARY_PATH=${PDI_INSTALL_DIR}/lib:${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+  #export LD_LIBRARY_PATH=${WORKING_DIR}/${PYTHON_ENV}/lib:${LD_LIBRARY_PATH}
+  #export LD_PRELOAD=${PDI_INSTALL_DIR}/lib64/libyaml.so # note: only for mac
+else
+  echo "Unknown option. accepted values are: old, new"
+  exit 1
+fi
+
+
+
+
 
 echo "PYTHONPATH=${PYTHONPATH}"
 echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
