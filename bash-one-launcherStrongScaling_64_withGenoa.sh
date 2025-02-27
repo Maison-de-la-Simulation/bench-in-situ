d121ef5de3164e5b86032fa1737b77b552389f47
Julian Auriac <julian.auriac@cea.fr>
2025-02-27 10:40:29 +0100

    Clean repository of test files 2

--- bash-one-launcherStrongScaling_64_withGenoa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for iteration in {1..1}; do
-    /bin/bash --wait -o resTest.out launcherStrongScaling_64_withGenoa.sh
-    echo iteration
-    grep -rw ${PWD}/results_withGenoa/NoDeisa/64 -e "RESULT" >> one-64-output_withGenoa.txt
-done
