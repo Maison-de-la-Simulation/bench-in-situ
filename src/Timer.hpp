a2887d10c28e65b8a0cebaa766e6000b1c95890f
Jacques Morice <jacques.morice@cea.fr>
2025-03-14 12:10:30 +0100

    testing 2

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -4,18 +4,22 @@
 #include <chrono>
 
 struct PerformanceTimer {
-  PerformanceTimer() : time_spent_in_io(0), time_spent_in_compute(0) {
+  PerformanceTimer() : time_spent_in_io(0), time_spent_in_compute(0), write_in_io(0), deepcopy_in_io(0) {
   }
 
   friend std::ostream& operator<<(std::ostream& os, const PerformanceTimer& obj) {
         
     os << "[RESULT] I/O: " << obj.time_spent_in_io.count() << " s" << std::endl;
     os << "[RESULT] Compute: " << obj.time_spent_in_compute.count() << " s" << std::endl;
+    os << "[RESULT] deepcopy I/O: " << obj.deepcopy_in_io.count() << " s" << std::endl;
+    os << "[RESULT] write I/O: " << obj.write_in_io.count() << " s" << std::endl;
     return os;
   }
 
   std::chrono::duration<double> time_spent_in_io;
   std::chrono::duration<double> time_spent_in_compute;
+  std::chrono::duration<double> write_in_io;
+  std::chrono::duration<double> deepcopy_in_io;
 };
 
 
