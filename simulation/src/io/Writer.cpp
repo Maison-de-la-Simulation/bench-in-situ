071fdef79df9e6aa5e823eb5cb55c7de293ed088
jacques.morice <jacques.morice@cea.Fr>
2025-04-25 11:30:48 +0200

    add native HDF5

--- simulation/src/io/Writer.cpp
+++ simulation/src/io/Writer.cpp
@@ -8,6 +8,9 @@
 #if defined(Euler_ENABLE_PDI)
 #include "WriterPDI.hpp"
 #endif
+#if defined(Euler_ENABLE_HDF5)
+#include "WriterHDF5.hpp"
+#endif
 #include "WriterTypes.hpp"
 #include "WriterVTK.hpp"
 
@@ -35,6 +38,14 @@ std::shared_ptr<WriterBase> WriterFactory::New(const UniformGrid& grid, const Pa
         ptr = std::make_shared<WriterPDI>(grid, params, prefix, variables);
 #else
         throw std::runtime_error("ARK has not been compiled with PDI support\n");
+#endif
+    }
+    else if (s2writer(type) == writer_t::hdf5)
+    {
+#if defined(Euler_ENABLE_HDF5)
+        ptr = std::make_shared<WriterHDF5>(grid, params, prefix, variables);
+#else
+        throw std::runtime_error("ARK has not been compiled with HDF5 support\n");
 #endif
     }
     else
--- simulation/src/io/WriterTypes.hpp
+++ simulation/src/io/WriterTypes.hpp
@@ -6,7 +6,8 @@ enum class writer_t : short
 {
     unknown,
     vtk,
-    pdi
+    pdi,
+    hdf5
 };
 
 inline
@@ -20,6 +21,10 @@ writer_t s2writer(const std::string& name)
     else if (name == "pdi")
     {
         type = writer_t::pdi;
+    }
+    else if (name == "hdf5")
+    {
+        type = writer_t::hdf5;
     }
     else
     {
