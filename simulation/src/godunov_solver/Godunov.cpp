6da3e8d17f30380ab31f43baaa74ab3c76269436
Julian Auriac <julian.auriac@cea.fr>
2025-07-04 11:15:36 +0200

    Add deep_copy() of data_device to copy_func() 42 - Retrieve Rstar_h, gamma, mmw 2

--- simulation/src/godunov_solver/Godunov.cpp
+++ simulation/src/godunov_solver/Godunov.cpp
@@ -335,8 +335,8 @@ void GodunovSolver::pdiExposeData()
                     "m_u_kokkos_view_dimensions", (void*)&m_u_kokkos_view_dimensions, PDI_OUT,
                     "m_u_host_kokkos_view_dimensions", (void*)&m_u_host_kokkos_view_dimensions, PDI_OUT,
                     "Rstar_h", (void*)&(code_units::constants::Rstar_h), PDI_OUT,
-                    "gamma", (void*)(m_params->thermo.gamma), PDI_OUT,
-                    "mmw", (void*)&(mmw), PDI_OUT,
+                    "gamma", (void*)&(m_params->thermo.gamma), PDI_OUT,
+                    "mmw", (void*)&(m_params->thermo.mmw), PDI_OUT,
                     NULL);
 
 #endif
