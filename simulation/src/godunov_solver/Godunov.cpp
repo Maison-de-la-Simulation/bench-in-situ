3331050df2126616fc0e280a6e413b1143d22d67
Julian Auriac <julian.auriac@cea.fr>
2025-07-07 17:23:49 +0200

    Add deep_copy() of data_device to copy_func() 59 - Retrieve sub-datasets 16

--- simulation/src/godunov_solver/Godunov.cpp
+++ simulation/src/godunov_solver/Godunov.cpp
@@ -352,12 +352,12 @@ void GodunovSolver::pdiExposeData()
 //    pdi_start[IY] = m_grid.m_nbCells[IY] * m_writer->m_mpi_coords[IY];
 //    pdi_start[IZ] = m_grid.m_nbCells[IZ] * m_writer->m_mpi_coords[IZ];
 
-//    std::ostringstream mpi_prefix;
-//    mpi_prefix << std::setw(3) << std::setfill('0') << tmp_rank;    
-//    std::string new_prefix(prefix);
-//    new_prefix.append("_r"+mpi_prefix.str());
+   std::ostringstream mpi_prefix;
+   mpi_prefix << std::setw(3) << std::setfill('0') << tmp_rank;    
+   std::string new_prefix(prefix);
+   new_prefix.append("_r"+mpi_prefix.str());
 
-//    int prefix_size = new_prefix.size() + 1;
+   int prefix_size = new_prefix.size() + 1;
 //    int nvar = 9;
     
    std::array<Real, 3> origin;
@@ -388,6 +388,8 @@ void GodunovSolver::pdiExposeData()
                     "ncell_local", pdi_ncells_local.data(), PDI_OUT,
                     // "start", pdi_start.data(), PDI_OUT,
                     "origin", origin.data(), PDI_OUT,
+                    "prefix_size", &prefix_size, PDI_OUT,
+                    "prefix", new_prefix.c_str(), PDI_OUT,
                     NULL);
 
 #endif
