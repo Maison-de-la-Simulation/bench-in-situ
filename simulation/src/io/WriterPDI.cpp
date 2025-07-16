4957df34f6acc2a7a3637a540db1de42e4e8228d
Julian Auriac <julian.auriac@cea.fr>
2025-07-16 09:42:29 +0200

    Add deep_copy() of data_device to copy_func() 73 - Add WriterGpuPDI template 11

--- simulation/src/io/WriterPDI.cpp
+++ simulation/src/io/WriterPDI.cpp
@@ -308,7 +308,7 @@ WriterPDI::WriterPDI(const UniformGrid& grid, const Params&,
     
 }
 
-std::string WriterPDI::getFilename(std::string const &prefix, Int outputId) {
+static std::string WriterPDI::getFilename(std::string const &prefix, Int outputId) {
   // write outputId in string outputNum
   std::ostringstream outputNum;
   outputNum << std::setw(std::numeric_limits<Int>::digits10);
