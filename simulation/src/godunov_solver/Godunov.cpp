f64ad58921732f82d50a7c94fcb2f5b8f1a00cd2
Julian Auriac <julian.auriac@cea.fr>
2025-09-04 16:18:20 +0200

    Attempt to circumvent event data_GPU_before bis 3

--- simulation/src/godunov_solver/Godunov.cpp
+++ simulation/src/godunov_solver/Godunov.cpp
@@ -328,6 +328,14 @@ void GodunovSolver::pdiExposeData()
         //         "m_u_host_kokkos_view_dimensions", (void*)&m_u_host_kokkos_view_dimensions, PDI_OUT,
         //         NULL);
 
+        PDI_multi_expose("",
+                "iStep", (void*)&(Super::m_iteration), PDI_OUT,
+                "m_u", (void*)(m_u.data()), PDI_OUT,
+                "m_u_host", (void*)(m_u_host.data()), PDI_OUT,
+                "m_u_kokkos_view_dimensions", (void*)&m_u_kokkos_view_dimensions, PDI_OUT,
+                "m_u_host_kokkos_view_dimensions", (void*)&m_u_host_kokkos_view_dimensions, PDI_OUT,
+                NULL);
+
         // printf(" _ %s\n",filename.c_str());
 
         m_writer->write(m_u_host, m_grid, Super::m_iteration, Super::m_t,
